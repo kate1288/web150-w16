be5398dccd3832489f109ad636351c9e5f55537d
kate1288 <kate1288@gmail.com>
2016-01-19 20:39:59 -0800

    Update script.js

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
 
-<h1>JavaScript Variables</h1>
 
-<p>Create a variable, assign a value to it, and display it:</p>
-
-<p id="demo"></p>
-
-<script>
 var myName = "kateBeaver";
-document.getElementById("demo").innerHTML = myName;
-</script>
-
-</body>
-</html>
 
